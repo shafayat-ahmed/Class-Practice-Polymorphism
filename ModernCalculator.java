104b52aeb1132356d3469468e12fec83ab75b590
shafayat-ahmed <ahmed.shafayat@gmail.com>
2018-12-06 00:27:10 -0500

    First Commit

---
+++ ModernCalculator.java
@@ -0,0 +1,8 @@
+
+public class ModernCalculator extends LandCalculator {
+
+	public int areaOfLand (int a, int b, int c) {
+		int total=((a+b+c)-1);
+		return total;
+	}
+}
---
+++ TestLand.java
@@ -0,0 +1,13 @@
+
+public class TestLand {
+
+	public static void main(String[] args) {
+		LandCalculator cal = new LandCalculator();
+		System.out.println(cal.areaOfLand(10, 5, 5));
+		
+		ModernCalculator cal1 = new ModernCalculator();
+		System.out.println(cal1.areaOfLand(10, 5, 5));
+		
+	}
+
+}
